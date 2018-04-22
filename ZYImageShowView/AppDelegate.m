181e6f953286743482b275c7b97478b6a38934f3
Young <mrzhouyoung@163.com>
2018-04-22 22:07:04 +0800

    Test2

--- ZYImageShowView/AppDelegate.m
+++ ZYImageShowView/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    NSLog(@"ABC");
+    NSLog(@"拉取文件");
     return YES;
 }
 
